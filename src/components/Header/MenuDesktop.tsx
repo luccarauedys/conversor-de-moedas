c768e9f90a3ddd84e90838ee204273328ba3b0ba
Lucca Rauédys <luccarauedys@outlook.com>
2023-05-05 14:01:06 -0300

    refactor: mudancas na organização dos componentes do header

--- src/components/Header/MenuDesktop.tsx
+++ src/components/Header/MenuDesktop.tsx
@@ -1,27 +1,43 @@
 import Link from "next/link";
-
 import { CurrencyDollar } from "@phosphor-icons/react";
 
 export default function MenuDesktop() {
+  const links = [
+    { href: "", title: "Home" },
+    { href: "", title: "Ferramentas" },
+    { href: "", title: "Recursos" },
+    { href: "", title: "Baixe o APP", highlight: true },
+  ];
+
+  const renderLinks = () => {
+    return links.map((link) => {
+      if (link.highlight) {
+        return (
+          <Link key={link.title} href={link.href} className="primary-button">
+            {link.title}
+          </Link>
+        );
+      }
+
+      return (
+        <Link
+          key={link.title}
+          href={link.href}
+          className="hover:scale-105 duration-300"
+        >
+          {link.title}
+        </Link>
+      );
+    });
+  };
+
   return (
     <div className="flex justify-between items-center text-white h-[80px]">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="uppercase font-semibold text-2xl">
           <CurrencyDollar size={40} weight="bold" />
         </Link>
-
-        <nav className="flex items-center gap-8">
-          <Link href="" className="hover:scale-105 duration-300">
-            Home
-          </Link>
-          <Link href="" className="hover:scale-105 duration-300">
-            Ferramentas
-          </Link>
-          <Link href="" className="hover:scale-105 duration-300">
-            Recursos
-          </Link>
-          <button className="primary-button">Baixe o aplicativo</button>
-        </nav>
+        <nav className="flex items-center gap-8">{renderLinks()}</nav>
       </div>
     </div>
   );
--- src/components/Header/MenuMobile.tsx
+++ src/components/Header/MenuMobile.tsx
@@ -5,12 +5,44 @@ import { CurrencyDollar, List, X } from "@phosphor-icons/react";
 export default function MenuMobile() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
+  const links = [
+    { href: "", title: "Home" },
+    { href: "", title: "Ferramentas" },
+    { href: "", title: "Recursos" },
+    { href: "", title: "Baixe o APP", highlight: true },
+  ];
+
+  const renderLinks = () => {
+    return links.map((link) => {
+      if (link.highlight) {
+        return (
+          <Link
+            key={link.title}
+            href={link.href}
+            className="primary-button bg-blue-600 hover:bg-blue-700  w-[180px] text-center mx-auto"
+          >
+            {link.title}
+          </Link>
+        );
+      }
+
+      return (
+        <Link
+          key={link.title}
+          href={link.href}
+          className="uppercase text-base font-medium text-center hover:scale-105 duration-300"
+        >
+          {link.title}
+        </Link>
+      );
+    });
+  };
+
   return (
     <div className="container flex justify-between text-white p-6">
       <Link href="/" className="uppercase text-xl font-semibold">
         <CurrencyDollar size={30} weight="bold" />
       </Link>
-
       <List
         weight="bold"
         onClick={() => setMenuIsOpen(true)}
@@ -24,30 +56,7 @@ export default function MenuMobile() {
             className="absolute top-6 right-6 text-3xl cursor-pointer hover:scale-110 duration-500"
           />
           <div className="grid grid-cols-1 mt-24 gap-8 max-w-[80%] mx-auto">
-            <Link
-              href="/"
-              className="uppercase text-base font-medium text-center hover:scale-105 duration-300"
-            >
-              Home
-            </Link>
-            <Link
-              href="/"
-              className="uppercase text-base font-medium text-center hover:scale-105 duration-300"
-            >
-              Ferramentas
-            </Link>
-            <Link
-              href="/"
-              className="uppercase text-base font-medium text-center hover:scale-105 duration-300"
-            >
-              Recursos
-            </Link>
-            <Link
-              href="/"
-              className="primary-button bg-blue-600 hover:bg-blue-700  w-[180px] text-center mx-auto"
-            >
-              Baixe o aplicativo
-            </Link>
+            {renderLinks()}
           </div>
         </div>
       )}
